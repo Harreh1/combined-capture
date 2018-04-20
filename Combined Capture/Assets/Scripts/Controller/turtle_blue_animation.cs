30569abf9f91be8b14642e9dfcac8a405edd1768
Harry <harry.l123@hotmail.com>
2018-04-20 11:16:08 +1000

    Added blue turtle animation

---
+++ Combined Capture/Assets/Scripts/Controller/turtle_blue_animation.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turtle_blue_animation : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    private bool avoiding;
+    private float avoidingMultipler;
+
+    public Transform moveSpots;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        waitTime = startWaitTime;
+        avoidingMultipler = 0;
+        moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!avoiding)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots.position, speed * Time.deltaTime);
+            Vector2 direction = moveSpots.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+            Debug.Log(transform.rotation.z);
+            if (angle >= 45 && angle < 135)
+            {
+                
+                animator.Play("blue_turtle_right");
+            }
+            else if (angle >= 135 && angle < 225)
+            {
+                animator.Play("blue_turtle_up");
+            }
+            else if (angle >= 225 && angle < 315)
+            {
+                animator.Play("blue_turtle_left");
+            }
+            else
+            {
+                animator.Play("blue_turtle_down");
+            }
+        }
+
+        if (Vector2.Distance(transform.position, moveSpots.position) < 0.5f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        sensors();
+    }
+
+    [Header("Sensors")]
+    public float sideSensorPos;
+    public float frontSensorAngle = 30;
+    private void sensors()
+    {
+
+        // Vector 2 of the moveSpot
+        Vector2 moveSpotsPos = new Vector2(moveSpots.position.x, moveSpots.position.y);
+        //Length of sesnor line
+        float sensorLength = 3500;
+
+        if (!avoiding) { avoidingMultipler = 0; }
+        avoiding = false;
+
+        //Front Sensor
+        Vector2 frontSensorPos = transform.position;
+        Vector2 frontSensorAim = transform.position + transform.up * sensorLength;
+        RaycastHit2D frontSensor = Physics2D.Raycast(frontSensorPos, (frontSensorAim - frontSensorPos).normalized, 3500);
+        if (frontSensor.collider != null && frontSensor.collider.CompareTag("Coll"))
+        {
+            Debug.DrawLine(frontSensorPos, frontSensor.point);
+            //moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            avoiding = true;
+            avoidingMultipler += 12000f;
+        }
+
+        //Front right sensor
+        Vector2 rightSensorPos = transform.position + transform.up + transform.right * sideSensorPos;
+        Vector2 rightSensorAim = transform.position + transform.up * sensorLength + transform.right * sideSensorPos;
+        RaycastHit2D rightSensor = Physics2D.Raycast(rightSensorPos, (rightSensorAim - rightSensorPos).normalized, sensorLength);
+        //Front left sensor 
+        Vector2 leftSensorPos = transform.position + transform.forward + transform.up - transform.right * sideSensorPos;
+        Vector2 leftSensorAim = transform.position + transform.up * sensorLength - transform.right * sideSensorPos;
+        RaycastHit2D leftSensor = Physics2D.Raycast(leftSensorPos, (leftSensorAim - leftSensorPos).normalized, sensorLength);
+        //Front-right
+        if (rightSensor.collider != null && rightSensor.collider.CompareTag("Coll"))
+        {
+            Debug.DrawLine(rightSensorPos, rightSensor.point);
+            avoiding = true;
+            avoidingMultipler += 6000f;
+
+        }
+        //Front-left
+        if (leftSensor.collider != null && leftSensor.collider.CompareTag("Coll"))
+        {
+            Debug.DrawLine(leftSensorPos, leftSensor.point);
+            avoiding = true;
+            avoidingMultipler += 6000f;
+        }
+
+        if (avoiding)
+        {
+            Vector2 direction = moveSpots.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;;
+            moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots.position * avoidingMultipler, speed * Time.deltaTime);
+        }
+        //Debug.DrawLine(leftSensorPos, leftSensorAim, Color.red);
+        //Debug.DrawLine(rightSensorPos, rightSensorAim, Color.red);
+        //Debug.DrawLine(frontSensorPos, frontSensorAim, Color.red);
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+     
+    }
+
+
+}
+
+
